9eb311f6c9cb4018af0af34eaec7bdf6003fa3f2
tz-hao <zzzth9810@Gmail.com>
2026-05-27 20:37:35 +0800

    提交task3

---
+++ learn/tz-hao/task3/next.config.mjs
@@ -0,0 +1,20 @@
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const projectRoot = dirname(fileURLToPath(import.meta.url));
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: [
+    "@demox-labs/aleo-wallet-adapter-base",
+    "@demox-labs/aleo-wallet-adapter-react",
+    "@demox-labs/aleo-wallet-adapter-reactui",
+    "aleo-adapters",
+    "@provablehq/sdk"
+  ],
+  turbopack: {
+    root: projectRoot
+  }
+};
+
+export default nextConfig;
